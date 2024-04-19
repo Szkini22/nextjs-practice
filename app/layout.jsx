6d20b7c1576e96f2cc3416d44c2e3d881aa54c1b
Catanzáró Tamás <catanzaro.tamas@eing.tigra.hu>
2024-04-19 17:10:38 +0200

    modifying layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,20 @@
-import Navbar from '@components/Navbar'
-import '@styles/globals.css'
+import Navbar from '@components/Navbar';
+import '@styles/globals.css';
 
 export const metadata = {
   title: 'Promptopia',
   description: 'Share AI prompts',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
-        <div className='main'>
-          <div className="gradient">
-          </div>
-        </div>
-
-        <main className="app">
-          <Navbar/>
+        <main className='w-[100vw] h-[100vh] bg-gradient-to-l from-[#FFC374] via-[#F9E897] to-[#7F9F80] flex flex-col items-center'>
+          <Navbar />
           {children}
         </main>
       </body>
     </html>
-  )
+  );
 }
