48a3974f64dbc412bcba7d32e98fc65cb6b60269
Catanzáró Tamás <catanzaro.tamas@eing.tigra.hu>
2024-04-19 17:10:09 +0200

    home page wip

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className='h-full w-full border-1 border-emerald-50 flex content-center'>
+      <div className='w-1/2 h-full flex justify-center items-center'>
+        <Image src='/assets/images/jugitjoga.jpg' width={100} height={300}></Image>
+      </div>
+      <div className='w-1/2 h-full flex justify-center items-center'>Right</div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
